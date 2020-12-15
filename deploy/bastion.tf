4a4d40d82ebe8ea58fa21ab728f007a371df00e9
Jordan Piel <JBenPiel@gmail.com>
2020-12-15 12:29:25 -0700

    Added bastion security groups

--- deploy/bastion.tf
+++ deploy/bastion.tf
@@ -25,12 +25,48 @@ resource "aws_iam_instance_profile" "bastion" {
 }
 
 resource "aws_instance" "bastion" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro"
-  user_data     = file("./templates/bastion/user-data.sh")
+  ami                  = data.aws_ami.amazon_linux.id
+  instance_type        = "t2.micro"
+  user_data            = file("./templates/bastion/user-data.sh")
+  iam_instance_profile = aws_iam_instance_profile.bastion.name
+  key_name             = var.bastion_key_name
+  subnet_id            = aws_subnet.public_a.id
+
+  vpc_security_group_ids = [
+    aws_security_group.bastion.id
+  ]
 
   tags = merge(
     local.common_tags,
     map("Name", "${local.prefix}-bastion")
   )
+}
+
+resource "aws_security_group" "bastion" {
+  description = "Control bastion inbound and outbound access"
+  name        = "${local.prefix}-bastion"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.common_tags
 }
