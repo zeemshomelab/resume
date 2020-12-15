4bf856a486692f69a3572f07f84192e995f5e27a
Jordan Piel <JBenPiel@gmail.com>
2020-12-15 16:48:45 -0700

    Update ELB to use defined TSL policy

--- deploy/dns.tf
+++ deploy/dns.tf
@@ -12,7 +12,7 @@ resource "aws_route53_record" "app" {
 }
 
 resource "aws_acm_certificate" "cert" {
-  domain_name       = aws_route53_record.app.fqdn
+  domain_name       = aws_route53_record.app.name
   validation_method = "DNS"
 
   tags = local.common_tags
@@ -23,17 +23,22 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  count   = length(aws_acm_certificate.cert.subject_alternative_names) + 1
-  name    = aws_acm_certificate.cert.domain_validation_options[count.index].resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options[count.index].resource_record_type
-  zone_id = data.aws_route53_zone.zone.zone_id
-  records = [
-    aws_acm_certificate.cert.domain_validation_options[count.index].resource_record_value
-  ]
-  ttl = "60"
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_record.app.zone_id
 }
 
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = aws_route53_record.cert_validation[*].fqdn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
