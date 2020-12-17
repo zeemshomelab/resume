de432e05ce0830613ddfd8c3b960a1dd71032c3c
Jordan Piel <JBenPiel@gmail.com>
2020-12-16 22:45:34 -0700

    Add CAA record for SSL

--- deploy/dns.tf
+++ deploy/dns.tf
@@ -11,8 +11,30 @@ resource "aws_route53_record" "app" {
   records = [aws_lb.resume.dns_name]
 }
 
+resource "aws_route53_record" "app_caa" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = data.aws_route53_zone.zone.name
+  type    = "CAA"
+  records = [
+    "0 issue \"amazon.com\"",
+    "0 issuewild \"amazon.com\""
+  ]
+  ttl = "300"
+}
+
 resource "aws_acm_certificate" "cert" {
   domain_name       = aws_route53_record.app.name
+  validation_method = "DNS"
+
+  tags = local.common_tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate" "cert_caa" {
+  domain_name       = aws_route53_record.app_caa.name
   validation_method = "DNS"
 
   tags = local.common_tags
@@ -38,7 +60,28 @@ resource "aws_route53_record" "cert_validation" {
   zone_id         = aws_route53_record.app.zone_id
 }
 
+resource "aws_route53_record" "cert_caa_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert_caa.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_record.app.zone_id
+}
+
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+
+resource "aws_acm_certificate_validation" "cert_caa" {
+  certificate_arn         = aws_acm_certificate.cert_caa.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_caa_validation : record.fqdn]
 }
