7a3c7531a00f826b140f47f01c4fccfff82e34ce
Jordan Piel <JBenPiel@gmail.com>
2021-06-12 11:32:16 -0600

    Update provider

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -36,7 +36,7 @@ resource "aws_ecs_cluster" "main" {
   tags = local.common_tags
 }
 
-resource "aws_iam_policy" "task_execution_role_policy" {
+resource "aws_iam_policy" "task_execution_policy" {
   name        = "${local.prefix}-task-exec-role-policy"
   path        = "/"
   description = "Allow retrieving images and adding to logs"
@@ -52,7 +52,7 @@ resource "aws_iam_role" "task_execution_role" {
 
 resource "aws_iam_role_policy_attachment" "task_execution_role" {
   role       = aws_iam_role.task_execution_role.name
-  policy_arn = aws_iam_policy.task_execution_role_policy.arn
+  policy_arn = aws_iam_policy.task_execution_policy.arn
 }
 
 resource "aws_iam_role" "app_iam_role" {
